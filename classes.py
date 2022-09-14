4dc1c587219b5f850cc8a8705aef35dfcfcbfdfb
kevin mills <kevin.mills@ivansinsurance.com>
2022-09-13 20:13:23 -0500

    new class files

--- classes.py
+++ classes.py
@@ -3,6 +3,10 @@ class Vehicle():
     def __init__(self, bodystyle="cart") -> None:
         self.n_bodystyle = bodystyle
 
+    def drive(self, speed):
+        self.mode = "driving"
+        self.speed = speed
+
 
 class Car(Vehicle):
     def __init__(self, enginetype) -> None:
@@ -11,6 +15,9 @@ class Car(Vehicle):
         self.doors = 4
         self.enginetype = enginetype
 
+    def drive(self, speed):
+        super().drive(speed)
+        print("Driving my ", self.enginetype, " car at", self.speed)
 
 class Truck(Vehicle):
     def __init__(self, enginetype) -> None:
@@ -18,6 +25,10 @@ class Truck(Vehicle):
         self.wheels = 4
         self.doors = 2
         self.enginetype = enginetype
+
+    def drive(self, speed):
+        super().drive(speed)
+        print("Driving my ", self.enginetype, " truck at", self.speed)
 
 
 class Motorcycle(Vehicle):
@@ -30,10 +41,19 @@ class Motorcycle(Vehicle):
         self.doors = 0
         self.enginetype = enginetype 
 
+    def drive(self, speed):
+        super().drive(speed)
+        print("Driving my ", self.enginetype, " motorcycle at", self.speed)
+
 
 car1 = Car("gas")
 car2 = Car("electric")
 mtc1 = Motorcycle("gas", True)
+
+car1.drive(30)
+car2.drive(40)
+mtc1.drive(50)
+
 print(" This vehicle is type: ", car1.n_bodystyle, ", with a ", car1.enginetype, " engine and has ", car1.doors, " door(s).")
 print(" This vehicle is type: ", car2.n_bodystyle, ", with a ", car2.enginetype, " engine and has ", car2.doors, " door(s).")
 print(" This vehicle is type: ", mtc1.n_bodystyle, ", with a ", mtc1.enginetype, " engine and has ", mtc1.doors, " door(s).")
